71dcd3f421af15a04b0b4abf7d417ed3f0285c8f
Benjamin Jasper <benjamin.jasper@itx.de>
2022-09-19 17:24:15 +0200

    fix: added workaround for TYPO3 bug

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -2,6 +2,13 @@
 
 return [
     'frontend' => [
+        'itx/typo3_graphql/request-object-fixer' => [
+            'target' => \Itx\Typo3GraphQL\Middleware\RequestObjectFixerMiddleware::class,
+            'before' => [
+                'itx/typo3_graphql/graphql-server',
+                'itx/typo3_graphql/graphql-cors'
+            ],
+        ],
         'itx/typo3_graphql/graphql-server' => [
             'target' => \Itx\Typo3GraphQL\Middleware\GraphQLServerMiddleware::class,
             'before' => [
