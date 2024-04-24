9a7ce37999419a6033e6880c1c149c7c7d8c08e6
Benjamin Jasper <benjamin.jasper@itx.de>
2024-04-24 08:51:10 +0200

    feat: added automatic cache flushing for filter options

--- ext_tables.php
+++ ext_tables.php
@@ -1,8 +1,13 @@
 <?php
+
+use Itx\Typo3GraphQL\Hook\DataHandlerHooks;
+
 defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(
     function()
     {
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['typo3_graphql'] = DataHandlerHooks::class;
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['typo3_graphql'] = DataHandlerHooks::class;
     }
 );
