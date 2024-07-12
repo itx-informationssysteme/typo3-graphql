dcc4140f8243f3dd3e223ac1d5942b628fffcddc
Benjamin Jasper <benjamin.jasper@itx.de>
2024-07-12 14:12:53 +0200

    fix: fixed missing info in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,10 +2,17 @@
 
 $EM_CONF['typo3_graphql'] = [
     'title' => 'TYPO3 GraphQL API',
-    'description' => '',
+    'description' => 'This extension provides a GraphQL API for TYPO3.',
     'category' => 'extension',
     'author' => 'Benjamin Jasper',
     'author_email' => 'benjamin.jasper@itx.de',
-    'state' => 'alpha',
+    'state' => 'beta',
     'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.1-12.9.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
 ];
