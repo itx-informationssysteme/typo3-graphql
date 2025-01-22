880cc6295bca3f41e0a41a88aab2fd4567466950
Benjamin Jasper <benjamin.jasper@itx.de>
2025-01-22 13:15:18 +0100

    fix: fixed date type input type

--- Classes/Types/Model/DateType.php
+++ Classes/Types/Model/DateType.php
@@ -7,6 +7,7 @@ use GraphQL\Type\Definition\Type;
 use SimPod\GraphQLUtils\Builder\InputFieldBuilder;
 use SimPod\GraphQLUtils\Builder\InputObjectBuilder;
 use Itx\Typo3GraphQL\Types\DateType as DateTypeScalar;
+use Itx\Typo3GraphQL\Types\TypeRegistry;
 
 class DateType extends ObjectType implements TypeNameInterface
 {
@@ -16,8 +17,8 @@ class DateType extends ObjectType implements TypeNameInterface
 
         $fields = [];
 
-        $fields[] = InputFieldBuilder::create('min', Type::nonNull(DateTypeScalar::$standardTypes))->build();
-        $fields[] = InputFieldBuilder::create('max', Type::nonNull(DateTypeScalar::$standardTypes))->build();
+        $fields[] = InputFieldBuilder::create('min', Type::nonNull(TypeRegistry::dateTime()))->build();
+        $fields[] = InputFieldBuilder::create('max', Type::nonNull(TypeRegistry::dateTime()))->build();
 
         $objectBuilder->setFields($fields);
         parent::__construct($objectBuilder->build());
