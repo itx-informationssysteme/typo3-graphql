c45049302fa0990f733cfc2434289aeeae945b64
pmy <patrik.marosvary@itx.de>
2025-01-28 13:34:10 +0100

    fix: Fixed oversight from last commit

--- Classes/Resolver/FilterResolver.php
+++ Classes/Resolver/FilterResolver.php
@@ -422,7 +422,7 @@ class FilterResolver
             $fieldPrefix = '';
         }
 
-        $queryBuilder->addSelectLiteral("$lastElementTable.$lastElement AS value")
+        $queryBuilder->addSelectLiteral($lastElementTable['lastElementTableAlias'] . ".$lastElement AS value")
                      ->from($tableName)
                      ->addSelectLiteral("COUNT($tableName.uid) AS resultCount")
                      ->groupBy("$fieldPrefix$lastElement")
