e7c8754996fc45dac1f4344281dc6df960977865
pmy <patrik.marosvary@itx.de>
2025-02-05 10:51:04 +0100

    fix: rangeFilterConfiguration now gets passed correctly again

--- Classes/Resolver/QueryResolver.php
+++ Classes/Resolver/QueryResolver.php
@@ -377,6 +377,6 @@ class QueryResolver
             $qb->andWhere(...$andExpressions);
         }
 
-        return [$discreteFilterConfigurations, $dateFilterConfiguration];
+        return [$discreteFilterConfigurations, $rangeFilterConfiguration, $dateFilterConfiguration];
     }
 }
