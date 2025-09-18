ea26c550769a8b3dbc44cd6eeae46406c6d5c957
pmy <patrik.marosvary@itx.de>
2025-09-18 12:51:53 +0200

    release: 3.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'it.x informationssysteme gmbh',
     'author_email' => 'typo-itx@itx.de',
     'state' => 'beta',
-    'version' => '1.0.2',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.1-12.9.99',
+            'typo3' => '13.4.1-13.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
